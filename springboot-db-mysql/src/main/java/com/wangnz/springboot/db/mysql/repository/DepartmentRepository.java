206cbaa182fcfb813c1d3833bd2273e3517f6e24
wangnz <5323700@qq.com>
2017-09-12 15:09:25 +0800

    update

--- springboot-db-mysql/src/main/java/com/wangnz/springboot/db/mysql/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wangnz.springboot.db.mysql.repository;
-
-import com.wangnz.springboot.db.mysql.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- springboot-hello/src/main/java/com/wangnz/springboot/hello/HelloApplication.java
+++ springboot-hello/src/main/java/com/wangnz/springboot/hello/HelloApplication.java
@@ -2,9 +2,8 @@ package com.wangnz.springboot.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloApplication {
     public static void main(String[] args) {
